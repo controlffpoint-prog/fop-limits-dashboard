8e3a6891811a9b50ae7ec5cf77f90151cff22b79
controlffpoint-prog <control.ff.point@gmail.com>
2026-09-19 11:37:21 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "FOP Limits Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
